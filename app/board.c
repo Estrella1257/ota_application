caa0a4e8357e23548ef0c4eb9118d447b9beffbb
Estrella1257 <ctan67074@gmail.com>
2025-11-18 17:52:48 +0800

    initialization(clock+led+freertos)

---
+++ app/board.c
@@ -0,0 +1,37 @@
+#include "stm32f4xx.h"
+#include "led.h"
+
+void board_lowlevel_init(void)
+{
+	NVIC_SetPriorityGrouping(NVIC_PriorityGroup_4);
+
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1,ENABLE);
+}
+
+const led_desc_t led0 =
+{
+    .clk_sourse = RCC_AHB1Periph_GPIOE,
+    .port = GPIOE,
+    .pin = GPIO_Pin_5,
+    .on_lvl = Bit_RESET,
+    .off_lvl = Bit_SET,
+};
+
+const led_desc_t led1 =
+{
+    .clk_sourse = RCC_AHB1Periph_GPIOE,
+    .port = GPIOE,
+    .pin = GPIO_Pin_6,
+    .on_lvl = Bit_RESET,
+    .off_lvl = Bit_SET,
+};
+
+const led_desc_t led2 =
+{
+    .clk_sourse = RCC_AHB1Periph_GPIOC,
+    .port = GPIOC,
+    .pin = GPIO_Pin_13,
+    .on_lvl = Bit_RESET,
+    .off_lvl = Bit_SET,
+};
---
+++ app/inc/main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32f4xx.h"
+#include "led.h"
+
+extern const led_desc_t led0;
+extern const led_desc_t led1;
+extern const led_desc_t led2;
+
+#endif
