9a9003218517292fd7807ea260baaba2256d8fdc
Estrella1257 <ctan67074@gmail.com>
2025-11-26 16:03:53 +0800

    uart+easylogger

--- app/main.c
+++ app/main.c
@@ -1,33 +1,54 @@
-#include <stdbool.h>
-#include <stdint.h>
-#include <string.h>
+#define LOG_TAG    "main"
+#include <elog.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "console.h"
 #include "main.h"
-#include "FreeRTOS.h"
-#include "task.h"
 
-static void blink_led(void *args)
-{
-	const led_desc_t *led=args;
-	led_init(led);
-	while (1)
-	{
-		led_on(led);
-		vTaskDelay(200);
-		led_off(led);
-		vTaskDelay(200);		
-	}
-	
+extern void board_lowlevel_init(void);
+static void test_elog(void);
+
+int main(void){
+    
+   	board_lowlevel_init();
+	uart_init();
+
+    setvbuf(stdout, NULL, _IONBF, 0); // 禁用 stdout 缓冲
+
+    /* initialize EasyLogger */
+    elog_init();
+	elog_set_text_color_enabled(true);
+
+    /* set EasyLogger log format */
+    elog_set_fmt(ELOG_LVL_ASSERT, ELOG_FMT_ALL);
+    elog_set_fmt(ELOG_LVL_ERROR, ELOG_FMT_LVL | ELOG_FMT_TAG );
+    elog_set_fmt(ELOG_LVL_WARN, ELOG_FMT_LVL | ELOG_FMT_TAG );
+    elog_set_fmt(ELOG_LVL_INFO, ELOG_FMT_LVL | ELOG_FMT_TAG );
+    elog_set_fmt(ELOG_LVL_DEBUG, ELOG_FMT_ALL & ~(ELOG_FMT_FUNC | ELOG_FMT_T_INFO | ELOG_FMT_P_INFO));
+    elog_set_fmt(ELOG_LVL_VERBOSE, ELOG_FMT_ALL);
+    /* start EasyLogger */
+    elog_start();
+
+    printf("EasyLogger started.\n");
+    test_elog();
+    while(1) {
+		
+    }
+    
+    return 0;
 }
 
-int main(void)
-{
-	xTaskCreate(blink_led,"blink_led",256,(void *)&led0,1,NULL);
-	xTaskCreate(blink_led,"blink_led",256,(void *)&led1,1,NULL);
-	xTaskCreate(blink_led,"blink_led",256,(void *)&led2,1,NULL);
-
-	vTaskStartScheduler();
- 
-	return 0;
+/**
+ * EasyLogger demo
+ */
+void test_elog(void) {
+    /* test log output for all level */
+    log_a("Hello EasyLogger!");
+    log_e("Hello EasyLogger!");
+    log_w("Hello EasyLogger!");
+    log_i("Hello EasyLogger!");
+    log_d("Hello EasyLogger!");
+    log_v("Hello EasyLogger!");
+    elog_raw("Hello EasyLogger!");
 }
-
                                               
